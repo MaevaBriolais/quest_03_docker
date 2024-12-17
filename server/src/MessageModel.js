80acf5aa888f73423fd2c2f3f75767182e7c9275
MaevaBriolais <briolais.maeva.pro@gmail.com>
2024-12-17 16:44:32 +0100

    Quest_03_docker

---
+++ server/src/MessageModel.js
@@ -0,0 +1,7 @@
+
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const MessageSchema = new Schema({
+  sentence: String
+});
+module.exports = mongoose.model("message", MessageSchema);
